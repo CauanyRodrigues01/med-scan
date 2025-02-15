86868d6080e0d85e4c8473ff1c6f94c809a372a3
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-02-15 12:02:21 -0300

    Adicionado funcao de traduzir o resultado

--- app/home.tsx
+++ app/home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { View, Image, ActivityIndicator, Alert, Modal } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
@@ -12,11 +12,13 @@ import { styles } from "@/styles/styleHome";
 import { Classification } from "@/components/Classification";
 import { CameraControls } from "@/components/CameraControls";
 import { classificationProps } from "@/components/Classification";
+import { translate } from "@vitalets/google-translate-api";
 
 export default function Index() {
   const [imageUri, setImageUri] = useState<string>("");
   const [loading, setLoading] = useState(false);
   const [results, setResults] = useState<classificationProps[]>([]);
+  const [translatedClassification, setTranslatedClassification] = useState<classificationProps[]>([]);
   const [facing, setFacing] = useState<CameraType>("back");
   const [modalVisible, setModalVisible] = useState(false);
   const [cameraPermission, requestCameraPermission] = useCameraPermissions();
@@ -83,6 +85,7 @@ export default function Index() {
   async function classifyImage(uri: string) {
     try {
       setResults([]);
+      setTranslatedClassification([]);  
       await tensorFlow.ready();
       const model = await mobilenet.load();
       console.log("Modelo MobileNet carregado");
@@ -96,19 +99,44 @@ export default function Index() {
       const tensorNormalized = resizedTensor.div(255.0);
       const result = await model.classify(tensorNormalized as tensorFlow.Tensor3D);
       console.log("Resultado da classificação:", result);
-      setResults(result);
+      setResults(result);  // Atualiza o estado com os resultados classificados
     } catch (error) {
       console.error("Erro ao classificar imagem:", error);
       Alert.alert("Erro", "Falha na classificação da imagem.");
     }
   }
 
+  // Sempre que`results` for atualizado, traduzimos automaticamente
+  useEffect(() => {
+    async function translateClassificationResults() {
+      try {
+        if (results.length > 0) {
+          console.log("Iniciando tradução...");
+          const translatedResults = await Promise.all(
+            results.map(async (result) => {
+              const translation = await translate(result.className, { to: "pt" });
+              return {
+                className: translation.text,
+                probability: result.probability,
+              };
+            })
+          );
+          setTranslatedClassification(translatedResults);
+          console.log("Resultados traduzidos:", translatedResults);
+        }
+      } catch (error) {
+        console.error("Erro ao traduzir os resultados:", error);
+      }
+    }
+    translateClassificationResults();
+  }, [results]);  // Dependência `results`, será executado sempre que `results` mudar
+
   return (
     <View style={styles.container}>
       <StatusBar translucent style="dark" />
       <Image source={{ uri: imageUri || "https://encurtador.com.br/Yd2Jg" }} style={styles.image} />
       <View style={styles.results}>
-        {results.map((res) => (
+        {translatedClassification.map((res) => (
           <Classification data={res} key={res.className} />
         ))}
       </View>
